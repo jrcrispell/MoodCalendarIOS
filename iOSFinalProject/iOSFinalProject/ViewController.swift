96178944e30254a5e1655b953b51b1f75c66fd06
Jason Crispell <jrcrispell@gmail.com>
2017-09-27 13:45:12 -0400

    First push

---
+++ iOSFinalProject/iOSFinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSFinalProject
+//
+//  Created by Jason Crispell on 9/27/17.
+//  Copyright © 2017 Crispell Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

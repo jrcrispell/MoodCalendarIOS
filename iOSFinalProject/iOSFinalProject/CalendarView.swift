874907f6f6396ea05e4da92857018bf0bd5e98af
Jason Crispell <jrcrispell@gmail.com>
2017-10-31 17:38:26 -0400

    Added hour lines in calendarView

--- iOSFinalProject/iOSFinalProject/CalendarView.swift
+++ iOSFinalProject/iOSFinalProject/CalendarView.swift
@@ -13,6 +13,26 @@ class CalendarView: UIView {
     
     var viewControllerDelegate: ViewControllerDelegate?
     
+    var hourBezier = UIBezierPath()
+    var halfHourBezier = UIBezierPath()
+    
+    var lineStartX: Double = 75
+    var lineEndX: Double = 600
+    var firstLineY: Double = 11
+    var hourVerticalPoints: Double = 48.7
+
+    
+    // Old globals
+//    var g_oneHourVerticalPoints: Double = 48.7
+//    var g_moodRectangleXPosition = 315
+//    var g_moodValueIndentation = 17
+//    var g_firstLineYPosition: Double = 11
+//    var g_moodRectangleWidth = 120
+//    var g_eventRectangleLeadingX: Double = 86
+//    var g_eventRectangleWidth: Double = 215
+//    var g_draggableLineHeight: Double = 10
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -24,6 +44,30 @@ class CalendarView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
+
+        UIColor.black.set()
+        
+        // Draw hour lines
+        for index in 0...23 {
+            
+            hourBezier.move(to: CGPoint(x: lineStartX, y: firstLineY + hourVerticalPoints * Double(index)))
+            hourBezier.addLine(to: CGPoint(x: lineEndX, y: firstLineY + hourVerticalPoints * Double(index)))
+            hourBezier.close()
+            hourBezier.stroke()
+            hourBezier.fill()
+        }
+        
+        UIColor.lightGray.set()
+        
+        for index in 0...23 {
+            
+            halfHourBezier.move(to: CGPoint(x: lineStartX, y: firstLineY + hourVerticalPoints / 2 + hourVerticalPoints * Double(index)))
+            halfHourBezier.addLine(to: CGPoint(x: lineEndX, y: firstLineY + hourVerticalPoints / 2 + hourVerticalPoints * Double(index)))
+            halfHourBezier.close()
+            halfHourBezier.stroke()
+            halfHourBezier.fill()
+        }
+
 
         if let daysEvents = viewControllerDelegate?.getDaysEvents() {
         for event in daysEvents {

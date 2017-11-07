3c23aa1cf11d29ba84bb103d640becf2d9a44d51
Jason Crispell <jrcrispell@gmail.com>
2017-11-07 17:15:00 -0500

    More progress on Logger View

--- iOSFinalProject/iOSFinalProject/LoggerViewController.swift
+++ iOSFinalProject/iOSFinalProject/LoggerViewController.swift
@@ -8,50 +8,65 @@
 
 import UIKit
 
-class LoggerViewController: UIViewController {
-
+class LoggerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var startTimePicker: UIDatePicker!
+    @IBOutlet weak var endTimePicker: UIDatePicker!
     @IBOutlet weak var navigationBar: UINavigationBar!
-    
     @IBOutlet weak var deleteButton: UIBarButtonItem!
-    
-    var editingActivity: CalendarActivity!
-    
-    var displayedDate = Date()
-
     @IBOutlet weak var dateLabel: UINavigationItem!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var moodPicker: UIPickerView!
+
+    
+    
+    // Incoming data
+    var editingActivity: CalendarActivity!
+    var displayedDate = Date()
+    var incomingStartTime: Double = 8
+    var incomingEndTime: Double = 9
+    
+    // Misc
+    let calendar = Calendar.current
+    let moodPickerData = ["10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if editingActivity == nil {
             deleteButton.isEnabled = false
+            moodPicker.selectRow(5, inComponent: 0, animated: true)
+        }
+        else {
+            incomingStartTime = editingActivity.startTime
+            incomingEndTime = editingActivity.endTime
+            descriptionField.text = editingActivity.activityDescription
+            moodPicker.selectRow(10 - editingActivity.moodScore, inComponent: 0, animated: true)
         }
         
+        startTimePicker.setDate(doubleToDate(time: incomingStartTime), animated: true)
+        endTimePicker.setDate(doubleToDate(time: incomingEndTime), animated: true)
+
+        // Navigation Bar
         navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
         navigationBar.isTranslucent = true
         navigationBar.isOpaque = true
         navigationBar.backgroundColor = UIColor.clear
         navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationBar.shadowImage = UIImage()
-        //navigationBar.tintColor = UIColor.clear
-        if (self.navigationController) != nil
-        {
-            print("exists")
-        }
-        else {
-            print("nope")
-        }
-        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMM d, yyyy"        
         dateLabel.title = dateFormatter.string(from: displayedDate)
         
         
     }
+    
+    func doubleToDate(time: Double) -> Date {
+        let hourFractional = time - Double(Int(time))
+        let dateComponents = DateComponents(calendar: calendar, hour: Int(time), minute: Int(60*hourFractional))
+        return calendar.date(from: dateComponents)!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - IBActions
@@ -64,6 +79,30 @@ class LoggerViewController: UIViewController {
     }
     
     @IBAction func deleteTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - Slider setup
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return moodPickerData[row]
+    }
+    
+    // Keyboard dismissals
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        descriptionField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        descriptionField.resignFirstResponder()
+        return true
     }
     
 

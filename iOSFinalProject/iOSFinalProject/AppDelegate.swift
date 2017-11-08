63b1811de14d481a5bcea6466522161b3a5e62e5
Jason Crispell <jrcrispell@gmail.com>
2017-11-08 12:21:46 -0500

    Added login view controller & firebase auth pods

--- iOSFinalProject/iOSFinalProject/AppDelegate.swift
+++ iOSFinalProject/iOSFinalProject/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         // Initialize Firebase Pod
-        FIRApp.configure()
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
---
+++ iOSFinalProject/iOSFinalProject/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  iOSFinalProject
+//
+//  Created by Jason Crispell on 11/8/17.
+//  Copyright © 2017 Crispell Apps. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func registerTapped(_ sender: Any) {
+    }
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    @IBAction func forgotTapped(_ sender: Any) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

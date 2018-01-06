c4604168e12e4b92fcd9adb8b648cfe2522a909e
Jason Crispell <jrcrispell@gmail.com>
2018-01-05 21:04:56 -0500

    Stats now calculate

--- iOSFinalProject/iOSFinalProject/StatsViewController.swift
+++ iOSFinalProject/iOSFinalProject/StatsViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class StatsViewController: UIViewController {
     
@@ -17,9 +19,64 @@ class StatsViewController: UIViewController {
     var oldSnapshot: UIImage!
 
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let ref = Database.database().reference()
+        let user = Auth.auth().currentUser
+        
+        let userRef = ref.child(user!.uid)
+        userRef.observeSingleEvent(of: .value) { (snapshot) in
+            guard let daysArray = snapshot.value as? [String:Any] else {return}
+            
+            var daysLogged = 0
+            var moodScores: [Int] = []
+            var moodScoreSum = 0
+            var hoursLogged = 0.0
+            
+            for day in daysArray {
+                if day.key == "Achievements" || day.key == "Experience" {continue}
+                else {
+                    daysLogged += 1
+                    guard let activitiesArray = daysArray[day.key] as? [String:Any] else {return}
+                    print(activitiesArray.description)
+                    for activity in activitiesArray {
+                        guard let valuesArray = activitiesArray[activity.key] as? [String:Any],
+                        let moodScore = valuesArray["moodScore"] as? Int,
+                        let startTime = valuesArray["startTime"] as? Double,
+                        let endTime = valuesArray["endTime"] as? Double else {return}
+                        hoursLogged += endTime - startTime
+                        moodScores.append(moodScore)
+                        moodScoreSum += moodScore
+                    }
+                }
+            }
+            self.daysCount.text = daysLogged.description
+            self.activitiesCount.text = moodScores.count.description
+            self.hoursCount.text = String(format: "%.2f", hoursLogged)
+            let moodScoreAverage = Double(moodScoreSum) / Double(moodScores.count)
+            self.moodAverage.text = String(format: "%.2f", moodScoreAverage)
+            
+            
+            
+
+            
+//            for day in daysArray {
+//                if day.key == "Achievements" || day.key == "Experience" {continue}
+//                else {
+//                    daysLogged += 1
+//                    let dayRef = userRef.child(day.key)
+//                    dayRef.observeSingleEvent(of: .value, with: { (snapshot) in
+//                        guard let activityArray = snapshot.value as? [String:Any] else {return}
+//
+//
+//                    })
+//                }
+//            }
+            self.daysCount.text = daysLogged.description
+        }
+        
         // Do any additional setup after loading the view.
     }
 
